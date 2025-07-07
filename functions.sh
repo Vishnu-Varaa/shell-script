a44d69174cf42a1a533bb7d0f2a6b2b0e717038d
Vishnu-Varaa <varadavishnupriya@gmail.com>
2025-07-07 14:55:19 +0530

    install mysqld - file modified

--- functions.sh
+++ functions.sh
@@ -8,14 +8,14 @@
 
 USERID=$(id -u)
 
-if[ $USERID -ne 0 ]
+if [ $USERID -ne 0 ]
 then
     echo "ERROR :: you must have sudo access to execute the script"
 fi
 
 dnf list installed mysql
 
-if[ $? -ne 0 ] 
+if [ $? -ne 0 ] 
 then
     echo "Installing mysql - start"
     dnf install mysql -y
